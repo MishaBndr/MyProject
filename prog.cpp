b7e6f53af9256af6f36826a89e87feb65754693e
MishaBndr <topotunumka@gmail.com>
2025-09-26 19:15:29 +0300

    Add files via upload

--- prog.cpp
+++ prog.cpp
@@ -13,21 +13,63 @@ int main() {
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
 
-    int a;
-    cin >> a;
-    if ((a % 4 == 0 && a % 100 != 0) || a % 400 == 0) {
-        cout << "Год высокостный.";
+    string colour_of_text;
+    string colour_of_background;
+    cout << "Введите желаемый цвет текста (красный, синий или зелёный): ";
+    cin >> colour_of_text;
+    cout << "Введите желаемый цвет фона (красный, синий или зелёный): ";
+    cin >> colour_of_background;
+
+    if (colour_of_text == "красный") {
+        cout << "\033[31m";
+        if (colour_of_background == "красный") {
+            cout << "Вывод отменён из-за сливающихся цветов!";
+            return 1;
+        }
+
+        else if (colour_of_background == "синий") {
+            cout << "\033[44m";
+        }
+
+        else if (colour_of_background == "зелёный") {
+            cout << "\033[42m";
+        }
+        cout << "Ceci est un texte de test de la console !";
     }
 
-    else {
-        cout << "Год не высокостный!";
+    else if (colour_of_text == "синий") {
+        cout << "\033[34m";
+        if (colour_of_background == "красный") {
+            cout << "\033[41m";
+        }
+
+        else if (colour_of_background == "синий") {
+            cout << "Вывод отменён из-за сливающихся цветов!";
+            return 1;
+        }
+
+        else if (colour_of_background == "зелёный") {
+            cout << "\033[42m";
+        }
+        cout << "Ceci est un texte de test de la console !";
     }
 
+    else if (colour_of_text == "зелёный") {
+        cout << "\033[32m";
+        if (colour_of_background == "красный") {
+            cout << "\033[41mCeci est un texte de test de la console !";
+        }
 
+        else if (colour_of_background == "синий") {
+            cout << "\033[44mCeci est un texte de test de la console !";
+        }
 
-
-
-
+        else if (colour_of_background == "зелёный") {
+            cout << "Вывод отменён из-за сливающихся цветов!";
+            return 1;
+        }
+        cout << "Ceci est un texte de test de la console !";
+    }
 
     return 0;
 }
